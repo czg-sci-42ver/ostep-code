ae6f7aac726eb829bf2ea66fa53a9862daa5b5ef
czg <czgsci@163.com>
2023-09-02 14:50:05 +0800

    update locks

--- threads-locks/compare-and-swap.c
+++ threads-locks/compare-and-swap.c
@@ -6,7 +6,17 @@ char compare_and_swap(int *ptr, int old, int new) {
     unsigned char ret;
     // Note that sete sets a ’byte’ not the word
     __asm__ __volatile__ (
-	" lock\n"
+    /*
+    https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html
+    1. here only use outside passed-in parameters as "InputOperands"
+    2. ignore this: https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html#x86-Operand-Modifiers
+        here `QImode` is enough for `unsigned char`.
+        - Above link is about "h" in '"xchg %h0, %b0" : "+a" (num)'
+    3. https://github.com/gcc-mirror/gcc/blob/master/gcc/config/i386/constraints.md from https://stackoverflow.com/a/34460377/21294350
+    4. "m" -> memory by https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html#Output-Operands
+    5. `"a" (old)` is due to `cmpxchg` compare with "a" register like rax.
+    */
+	" lock\n\t"
 	" cmpxchgl %2,%1\n"
 	" sete %0\n"
 	: "=q" (ret), "=m" (*ptr)
